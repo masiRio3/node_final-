709d12a0961177c737eace6a40e0441b238396b7
masirio3 <demasimauri@gmail.com>
2022-02-12 11:34:43 -0300

    first commit

---
+++ src/entity/user.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn} from 'typeorm';
+
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class User {
+
+    @Field()
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Field()
+    @Column()
+    fullName!: string
+
+    @Field()
+    @Column()
+    email!: string
+
+    @Field()
+    @Column()
+    password!: string
+
+    @Field()
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt!: string
+}
